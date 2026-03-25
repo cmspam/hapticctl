1a9d8034ad889f558a359fa6c06e8a4a4a0f94d7
cmspam <cmspam>
2026-03-26 04:49:18 +0900

    Fix build for nix

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
           version = "0.1.0";
           src = ./.;
           format = "pyproject";
-          nativeBuildInputs = [ setuptools wheel ];
+          nativeBuildInputs = [ pkgs.python3Packages.setuptools pkgs.python3Packages.wheel ];
           meta = with pkgs.lib; {
             description = "Control haptic touchpad feedback intensity on Linux via HID";
             homepage = "https://github.com/cmspam/hapticctl";
